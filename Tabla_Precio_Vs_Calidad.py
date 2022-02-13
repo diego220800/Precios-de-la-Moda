89815d55cdfb8da02cbc787eb41fc5ef50337f81
ToineLinares <73902967+ToineLinares@users.noreply.github.com>
2022-02-13 01:05:30 -0500

    Add files via upload

---
+++ Tabla_Precio_Vs_Calidad.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+r = np.loadtxt('PrecioCalidad.csv', delimiter = ",")
+precios = r[:,0]
+c_1 = np.zeros((10,51))
+for j in range(len(c_1)):
+    for i in range(len(r[0])):
+        if i < len(c_1[0]):
+            
+            c_1[j,i] = r[j,i+1]
+        else:
+            break
